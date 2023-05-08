04e32d0ee3e7bded1a14033cec9fb49965510dd0
LSS(임상수) <94133002+Ss0o@users.noreply.github.com>
2023-05-08 13:34:45 +0900

    Delete DataType.java

--- DataType.java
+++
@@ -1,8 +0,0 @@
-public class DataType {
-    public static void main(String[] args) {
-        int a=22;
-        String ch1="He is my friend.", ch2="Nice to meet you.";
-        System.out.println(ch1+" "+ch2+" "+a+"years old");
-
-    }
-}
