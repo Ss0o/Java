108278b92da7c5a580ee9ae7763f0e98291a9a05
Sangsoo <sangsoo3510@gmail.com>
2023-05-08 13:11:43 +0900

    data type

---
+++ DataType.java
@@ -0,0 +1,7 @@
+public class DataType {
+    public static void main(String[] args) {
+        int a=11;
+        String ch1="He is my friend.", ch2="Nice to meet you.";
+        System.out.println(ch1+" "+ch2+" "+a);
+    }
+}
