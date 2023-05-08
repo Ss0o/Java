84be707376f3559745a9e0cbe558b08a44a96ddb
Sangsoo <sangsoo3510@gmail.com>
2023-05-08 13:30:11 +0900

    test111

--- JAVA/Chapter01/test01.java
+++ JAVA/Chapter01/test01.java
@@ -2,6 +2,6 @@ package JAVA.Chapter01;
 
 public class test01 {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello World");
     }
 }
