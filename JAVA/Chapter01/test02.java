5174c6bb6101055f3c053f5c8dffec2d775e4571
Sangsoo <sangsoo3510@gmail.com>
2023-05-08 13:32:34 +0900

    test222

--- JAVA/Chapter01/test02.java
+++ JAVA/Chapter01/test02.java
@@ -2,6 +2,6 @@ package JAVA.Chapter01;
 
 public class test02 {
     public static void main(String[] args) {
-        System.out.println("Nice to meet you");
+        System.out.println("Nice to meet you S");
     }
 }
